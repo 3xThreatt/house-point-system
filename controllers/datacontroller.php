e99320d11d1b459993dd7cbef42fa445a6f933ea
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2021-07-10 17:16:12 +1200

    1.2.3

--- controllers/datacontroller.php
+++ controllers/datacontroller.php
@@ -4,30 +4,30 @@ include("controllers/config.php");
 //Points
 $hTakaheQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=1";
 $hTakaheResult = mysqli_query($con, $hTakaheQueryPoints);
-$hTakaheRow = mysqli_fetch_array($hTakaheResult);
+$hTakaheRow = mysqli_fetch_row($hTakaheResult);
 
 $hTiekeQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=2";
 $hTiekeResult = mysqli_query($con, $hTiekeQueryPoints);
-$hTiekeRow = mysqli_fetch_array($hTiekeResult);
+$hTiekeRow = mysqli_fetch_row($hTiekeResult);
 
 $hTaraItiQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=3";
 $hTaraItiResult = mysqli_query($con, $hTaraItiQueryPoints);
-$hTaraItiRow = mysqli_fetch_array($hTaraItiResult);
+$hTaraItiRow = mysqli_fetch_row($hTaraItiResult);
 
 $hKeaQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=4";
 $hKeaResult = mysqli_query($con, $hKeaQueryPoints);
-$hKeaRow = mysqli_fetch_array($hKeaResult);
+$hKeaRow = mysqli_fetch_row($hKeaResult);
 
 $hKokakoQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=1";
 $hKokakoResult = mysqli_query($con, $hKokakoQueryPoints);
-$hKokakoRow = mysqli_fetch_array($hKokakoResult);
+$hKokakoRow = mysqli_fetch_row($hKokakoResult);
 
 
-$hTakaheDesc = $hTakaheRow
-$hTiekeDesc = $hTiekeRow;
-$hTaraItiDesc = $hTaraItiRow;
-$hKeaDesc = $hKeaRow;
-$hKokakoDesc = $hKokakoRow;
+$hTakaheDesc = $hTakaheRow[0];
+$hTiekeDesc = $hTiekeRow[0];
+$hTaraItiDesc = $hTaraItiRow[0];
+$hKeaDesc = $hKeaRow[0];
+$hKokakoDesc = $hKokakoRow[0];
 
 echo $hTakaheDesc;
 echo $hTiekeDesc;
