0572b504bd823d8bca63d9c9dbce992bb591d69f
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2021-07-10 19:28:49 +1200

    Version 1.2.5

--- controllers/datacontroller.php
+++ controllers/datacontroller.php
@@ -2,31 +2,56 @@
 include("controllers/config.php");
 
 //Points
-$hTakaheQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=1";
+$hTakaheQueryPoints = "SELECT * FROM house WHERE HouseID=1";
 $hTakaheResult = mysqli_query($con, $hTakaheQueryPoints);
 $hTakaheRow = mysqli_fetch_row($hTakaheResult);
 
-$hTiekeQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=2";
+
+$hTiekeQueryPoints = "SELECT * FROM house WHERE HouseID=2";
 $hTiekeResult = mysqli_query($con, $hTiekeQueryPoints);
 $hTiekeRow = mysqli_fetch_row($hTiekeResult);
 
-$hTaraItiQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=3";
+
+$hTaraItiQueryPoints = "SELECT * FROM house WHERE HouseID=3";
 $hTaraItiResult = mysqli_query($con, $hTaraItiQueryPoints);
 $hTaraItiRow = mysqli_fetch_row($hTaraItiResult);
 
-$hKeaQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=4";
+
+$hKeaQueryPoints = "SELECT * FROM house WHERE HouseID=4";
 $hKeaResult = mysqli_query($con, $hKeaQueryPoints);
 $hKeaRow = mysqli_fetch_row($hKeaResult);
 
-$hKokakoQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=5";
+
+$hKokakoQueryPoints = "SELECT * FROM house WHERE HouseID=5";
 $hKokakoResult = mysqli_query($con, $hKokakoQueryPoints);
 $hKokakoRow = mysqli_fetch_row($hKokakoResult);
 
 
-$hTakaheDesc = $hTakaheRow[0];
-$hTiekeDesc = $hTiekeRow[0];
-$hTaraItiDesc = $hTaraItiRow[0];
-$hKeaDesc = $hKeaRow[0];
-$hKokakoDesc = $hKokakoRow[0];
+
+$hTakaheID = $hTakaheRow[0];
+$hTakakeName = $hTakaheRow[1];
+$hTahakeColour = $hTakaheRow[2];
+$hTakahePoint = $hTakaheRow[3];
+
+$hTiekeID = $hTiekeRow[0];
+$hTiekeName = $hTiekeRow[1];
+$hTiekeColour = $hTiekeRow[2];
+$hTiekePoint = $hTiekeRow[3];
+
+$hTaraIteID =  $hTaraItiRow[0];
+$hTaraIteName = $hTaraItiRow[1];
+$hTaraIteColour = $hTaraItiRow[2];
+$hTaraItiPoint = $hTaraItiRow[3];
+
+$hKeaID = $hKeaRow[0];
+$hKeaName = $hKeaRow[1];
+$hKeaColour = $hKeaRow[2];
+$hKeaPoint = $hKeaRow[3];
+
+$hKokakoID = $hKokakoRow[0];
+$hKokakoName = $hKokakoRow[1];
+$hKokakoColour = $hKokakoRow[2];
+$hKokakoPoint = $hKokakoRow[3];
+
 
 ?>
