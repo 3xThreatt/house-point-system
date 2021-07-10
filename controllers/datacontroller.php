577193e7a6776420c66b40929a10a3d7d76b1ab3
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2021-07-10 16:55:47 +1200

    version 1

---
+++ controllers/datacontroller.php
@@ -0,0 +1,38 @@
+<?php
+include("controllers/config.php");
+
+//Points
+$hTakaheQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=1";
+$hTakaheResult = mysql_query($con, $hTakaheQueryPoints);
+$hTakaheRow = mysql_fetch_array($hTakaheResult);
+
+$hTiekeQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=2";
+$hTiekeResult = mysql_query($con, $hTiekeQueryPoints);
+$hTiekeRow = mysql_fetch_array($hTiekeResult);
+
+$hTaraItiQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=3";
+$hTaraItiResult = mysql_query($con, $hTaraItiQueryPoints);
+$hTaraItiRow = mysql_fetch_array($hTaraItiResult);
+
+$hKeaQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=4";
+$hKeaResult = mysql_query($con, $hKeaQueryPoints);
+$hKeaRow = mysql_fetch_array($hKeaResult);
+
+$hKokakoQueryPoints = "SELECT HousePoints FROM house WHERE HouseID=1";
+$hKokakoResult = mysql_query($con, $hKokakoQueryPoints);
+$hKokakoRow = mysql_fetch_array($hKokakoResult);
+
+
+$hTakaheDesc = $hTakaheRow['content']
+$hTiekeDesc = $hTiekeRow['content']
+$hTaraItiDesc = $hTaraItiRow['content']
+$hKeaDesc = $hKeaRow['content']
+$hKokakoDesc = $hKokakoRow['content']
+
+echo $hTakaheDesc;
+echo $hTiekeDesc;
+echo $hTaraItiDesc;
+echo $hKeaDesc;
+echo $hKokakoDesc;
+
+?>
